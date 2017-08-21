ad87d20d7c29a13b797fa152bdff24301d001bd2
David Sowerby <david.sowerby@virgin.net>
2017-08-21 20:59:38 +0100

    Initial extract from Krail core

---
+++ src/main/java/uk/q3c/krail/persist/inmemory/InMemoryLabelKey.java
@@ -0,0 +1,10 @@
+package uk.q3c.krail.persist.inmemory;
+
+import uk.q3c.krail.i18n.I18NKey;
+
+/**
+ * Created by David Sowerby on 21 Aug 2017
+ */
+public enum InMemoryLabelKey implements I18NKey {
+    In_Memory
+}
---
+++ src/main/java/uk/q3c/krail/persist/inmemory/store/InMemoryDescriptionKey.java
@@ -0,0 +1,10 @@
+package uk.q3c.krail.persist.inmemory.store;
+
+import uk.q3c.krail.i18n.I18NKey;
+
+/**
+ * Created by David Sowerby on 21 Aug 2017
+ */
+public enum InMemoryDescriptionKey implements I18NKey {
+    Data_is_held_in_memory
+}
